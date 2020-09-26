b9cd3b1f9777cd934f467d7a58c14a601ada8999
dushuanglong <dushuanglong@bytedance.com>
2020-09-26 23:56:52 +0800

    fix: add device test

--- examples/screenshare/main.go
+++ examples/screenshare/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/pion/mediadevices/pkg/log"
+	"net/http"
 
 	"github.com/pion/mediadevices"
-	"github.com/pion/mediadevices/examples/internal/signal"
 	"github.com/pion/mediadevices/pkg/codec"
 	"github.com/pion/mediadevices/pkg/codec/vpx"       // This is required to use VP8/VP9 video encoder
 	_ "github.com/pion/mediadevices/pkg/driver/screen" // This is required to register screen capture adapter
@@ -15,7 +17,7 @@ import (
 )
 
 func main() {
-	addr := flag.String("address", ":50000", "Address to host the HTTP server on.")
+	addr := flag.String("address", "127.0.0.1:50000", "Address that the HTTP server is hosted on.")
 	flag.Parse()
 	config := webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
@@ -26,17 +28,11 @@ func main() {
 	}
 
 
-	// Exchange the offer/answer via HTTP
-	offerChan, answerChan := signal.MustSignalViaHTTP(*addr)
-	// Wait for the remote SessionDescription
-	offer := <-offerChan
-	fmt.Printf("pub receive offer: %+v\n", offer)
 
 	// Create a new RTCPeerConnection
 	mediaEngine := webrtc.MediaEngine{}
-	if err := mediaEngine.PopulateFromSDP(offer); err != nil {
-		panic(err)
-	}
+	mediaEngine.RegisterDefaultCodecs()
+
 	setttingEngine := webrtc.SettingEngine{}
 	setttingEngine.LoggerFactory = log.CustomLoggerFactory{}
 	api := webrtc.NewAPI(webrtc.WithMediaEngine(mediaEngine), webrtc.WithSettingEngine(setttingEngine))
@@ -86,27 +82,56 @@ func main() {
 		}
 	}
 
-	// Set the remote SessionDescription
-	err = peerConnection.SetRemoteDescription(offer)
+	// Create an offer
+	offer, err := peerConnection.CreateOffer(nil)
 	if err != nil {
 		panic(err)
 	}
-
-	// Create an answer
-	answer, err := peerConnection.CreateAnswer(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	answerChan <- answer
+	fmt.Printf("\noriginal offer: %+v\n", offer)
 
 	// Sets the LocalDescription, and starts our UDP listeners
-	err = peerConnection.SetLocalDescription(answer)
+	err = peerConnection.SetLocalDescription(offer)
 	if err != nil {
 		panic(err)
 	}
 
-	// Output the answer in base64 so we can paste it in browser
-	fmt.Println(signal.Encode(answer))
+	// Exchange the offer for the answer
+	answer := mustSignalViaHTTP(offer, *addr)
+	fmt.Printf("answer: %+v\n", answer)
+
+	// Set the remote SessionDescription
+	err = peerConnection.SetRemoteDescription(answer)
+	if err != nil {
+		panic(err)
+	}
+
 	select {}
+}
+
+// mustSignalViaHTTP exchange the SDP offer and answer using an HTTP Post request.
+func mustSignalViaHTTP(offer webrtc.SessionDescription, address string) webrtc.SessionDescription {
+	b := new(bytes.Buffer)
+	err := json.NewEncoder(b).Encode(offer)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.Post("http://"+address, "application/json; charset=utf-8", b)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		closeErr := resp.Body.Close()
+		if closeErr != nil {
+			panic(closeErr)
+		}
+	}()
+
+	var answer webrtc.SessionDescription
+	err = json.NewDecoder(resp.Body).Decode(&answer)
+	if err != nil {
+		panic(err)
+	}
+
+	return answer
 }
